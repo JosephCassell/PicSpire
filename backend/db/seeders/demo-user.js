9afd18f7a722a89c7eb8f5ca3f61df7c65e5bb5e
Joseph <yosefcassell@gmail.com>
2024-04-09 12:29:07 -0700

    aws

--- backend/db/seeders/demo-user.js
+++ backend/db/seeders/demo-user.js
@@ -2,6 +2,7 @@
 
 const bcrypt = require("bcryptjs");
 const { User } = require('../models');
+
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
@@ -46,7 +47,7 @@ module.exports = {
   async down (queryInterface, Sequelize) {
     options.tableName = 'Users';
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
+    return queryInterface.bulkDelete({ tableName: 'Users', schema: process.env.SCHEMA },  {
       username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
     }, {});
   }
