b7cd13c861e241994b3bc2404b03a98f4d83a03a
Joseph <yosefcassell@gmail.com>
2024-04-08 19:17:46 -0700

    render

--- backend/db/seeders/demo-user.js
+++ backend/db/seeders/demo-user.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Users', [
+    await queryInterface.bulkInsert({ tableName: 'Users', schema: process.env.SCHEMA }, [
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
