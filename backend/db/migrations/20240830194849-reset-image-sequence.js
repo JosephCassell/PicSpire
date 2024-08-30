ad7f4e633186a1ccae2afe3d88fe3dd51be60190
Joseph <yosefcassell@gmail.com>
2024-08-30 13:17:03 -0700

    render

--- backend/db/migrations/20240830194849-reset-image-sequence.js
+++ backend/db/migrations/20240830194849-reset-image-sequence.js
@@ -4,8 +4,8 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up() {
   },
-  async down(queryInterface, Sequelize) {
+  async down() {
   }
 };
