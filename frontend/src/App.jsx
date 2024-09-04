f8be615d26dafa138648fda1f8b75c20f6cd764b
Joseph <yosefcassell@gmail.com>
2024-09-04 11:38:59 -0700

    favicon3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import * as sessionActions from './store/session';
 import LandingPage from './components/LandingPage/LandingPage';
 import ProfilePage from './components/ProfilePage/ProfilePage';
 import FeedComponent from './components/FeedPage/FeedPage';
+
 function Layout() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
