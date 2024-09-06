60b3f2d9b3d336aac07ab3b891b2bd07ba957ae9
Joseph <yosefcassell@gmail.com>
2024-09-06 12:20:04 -0700

    css updates

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -49,7 +49,7 @@ const LandingPage = () => {
                 <ul>
                     <li>
                         <div className='list-item-container-1'>
-                            <h1 class='title'>Welcome to PicSpire</h1>
+                            <h1 className='title'>Welcome to PicSpire</h1>
                             <p className='catchphrase'>Inspire with your pics!</p>
                             <div className='image-grid'>
                                 <img src={selfieImage} alt="Moments" />
